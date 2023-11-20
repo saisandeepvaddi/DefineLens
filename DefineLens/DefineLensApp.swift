ac4d59137024ecd3e42be77b94082182c36f06e0
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2023-11-19 21:06:24 -0800

    Initial Commit

---
+++ DefineLens/DefineLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefineLensApp.swift
+//  DefineLens
+//
+//  Created by Sai Sandeep Vaddi on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DefineLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
