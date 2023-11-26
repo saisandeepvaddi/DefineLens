006b83b7d2739cc95187f072e4c6e0ea64f0c84c
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2023-11-26 12:23:00 -0800

    Update button text with word to check

--- DefineLens/ContentView.swift
+++ DefineLens/ContentView.swift
@@ -10,38 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var cameraManager = CameraManager()
     @StateObject var appState = AppState()
-    @State var clickedWord: String = ""
+    @State private var navigateToDefinition = false
+    @State private var recognizedWord: String?
     var body: some View {
-        ZStack {
-            CameraPreview(cameraManager: cameraManager)
-                .edgesIgnoringSafeArea(.all)
+        NavigationView {
+            ZStack {
+                CameraPreview(cameraManager: self.cameraManager)
+                    .edgesIgnoringSafeArea(.all)
 
-            if let previewLayer = cameraManager.previewLayer {
-                BoundingBoxesView(observations: cameraManager.textObservations, previewLayer: previewLayer, onWordChange: { newWord in
-                    appState.word = newWord
-                })
-                .edgesIgnoringSafeArea(.all)
-                .environmentObject(appState)
-            }
+                if let previewLayer = cameraManager.previewLayer {
+                    BoundingBoxesView(observations: self.cameraManager.textObservations, previewLayer: previewLayer, onWordChange: { newWord in
+                        self.appState.word = newWord
+                    })
+                    .edgesIgnoringSafeArea(.all)
+                    .environmentObject(self.appState)
+                }
 
-            Image(systemName: "circle")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 15, height: 15)
-                .foregroundColor(.red)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                Image(systemName: "circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 15, height: 15)
+                    .foregroundColor(.red)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
 
-            VStack {
-                Spacer()
-                Button("Snap") {
-                    snapWord()
+                VStack {
+                    Spacer()
+                    Button(action: {
+                        self.snapWord()
+                    }) {
+                        Text(self.appState.word ?? "Check")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .cornerRadius(20)
+                    }.padding()
+                }
+                NavigationLink(destination: DefinitionView(word: self.appState.word), isActive: self.$navigateToDefinition) {
+                    EmptyView()
                 }
             }
         }
     }
 
     func snapWord() {
-        print("Word: \(appState.word)")
+        if let word = appState.word {
+            self.navigateToDefinition = true
+            self.recognizedWord = word
+        }
 
         // Logic to get the word under the crosshair and print it
     }
